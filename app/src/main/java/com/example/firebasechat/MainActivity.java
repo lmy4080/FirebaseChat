7b2e0bf67b2c0790bf03d620fd7711f9e6b44ce2
mingyoung <tkfkd1212!>
2019-12-23 04:08:33 +0900

    added a var for recyclerview to activity_main.xml and created a viewholder class for the recyclerview on main activity

--- app/src/main/java/com/example/firebasechat/MainActivity.java
+++ app/src/main/java/com/example/firebasechat/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.firebasechat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMessageRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageRecyclerView = findViewById(R.id.message_recycler_view);
+    }
+
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView messageTextView;
+        ImageView messageImageView;
+        TextView messengerTextView;
+        CircleImageView messengerImageView;
+
+        public MessageViewHolder(View v) {
+            super(v);
+            messageTextView = v.findViewById(R.id.messageTextView);
+            messageImageView = v.findViewById(R.id.messageImageView);
+            messengerTextView = v.findViewById(R.id.messengerTextView);
+            messengerImageView = v.findViewById(R.id.messengerImageView);
+        }
+
     }
 }
