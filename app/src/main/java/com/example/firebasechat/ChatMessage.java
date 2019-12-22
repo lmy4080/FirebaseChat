2e3d94d158463462a2a88811e1d2a6c08fa5c710
mingyoung <tkfkd1212!>
2019-12-23 03:47:49 +0900

    created a message model for the recycler view on main activity

---
+++ app/src/main/java/com/example/firebasechat/ChatMessage.java
@@ -0,0 +1,60 @@
+package com.example.firebasechat;
+
+public class ChatMessage {
+    private String id; // db에 저장할 id
+    private String text; // 메시지
+    private String name; // 이름
+    private String photoUrl; // 프로필 사진 경로
+    private String imageUrl; // 첨부 이미지 경로
+
+    public ChatMessage() {
+
+    }
+
+    public ChatMessage(String text, String name, String photoUrl, String imageUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
