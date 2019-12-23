7ef144313188b60626d5e75e717a9cab067282b9
mingyoung <tkfkd1212!>
2019-12-24 01:07:03 +0900

    implemented that the app receives a push-data from firebase cloud messaging

---
+++ app/src/main/java/com/example/firebasechat/MyFcmService.java
@@ -0,0 +1,17 @@
+package com.example.firebasechat;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFcmService extends FirebaseMessagingService {
+    public static final String TAG = MyFcmService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // 여기서 FCM 메시지를 처리
+        Log.d(TAG, "onMessageReceived ID: " + remoteMessage.getMessageId());
+        Log.d(TAG, "onMessageReceived DATA: " + remoteMessage.getData());
+    }
+}
